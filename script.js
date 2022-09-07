d4e413c9f7d00d4bca5a5d39a41bf75e502faceb
Bidur Sahoo <78256127+Bidursahoo@users.noreply.github.com>
2022-09-07 14:42:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+const billTotalInputDiv = document.getElementById("billTotalInput")
+
+
+const tipInputDiv = document.getElementById("tipInput")
+
+
+const numberOfPeopleDiv = document.getElementById("numberOfPeople")
+
+
+const perPersonTotalDiv = document.getElementById("perPersonTotal")
+
+
+let noOfPeople = Number(numberOfPeopleDiv.innerText)
+
+
+const calculateBill = () => {
+    const billTotal = Number(billTotalInputDiv.value)
+    const tipInput = Number(tipInputDiv.value)
+    let tipPercent = tipInput/100;
+    let tipAmt = tipPercent*billTotal
+    let totalBill = (billTotal+tipAmt)/noOfPeople;
+    perPersonTotalDiv.innerText = totalBill.toFixed(2)
+}
+const increasePeople = () => {
+    noOfPeople++
+    numberOfPeopleDiv.innerText = noOfPeople
+    calculateBill()
+}
+const decreasePeople =() => {
+    if(noOfPeople ==1){
+        return
+    }
+    noOfPeople-=1;
+    numberOfPeopleDiv.innerText = noOfPeople
+    calculateBill()
+}
